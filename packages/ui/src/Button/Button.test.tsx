44d23f4c1e841901fca89b0c8f84265f9e37ec28
MrX8503 <viet_tran@hermanmiller.com>
2023-08-18 11:03:37 -0400

    test(button): update snaps

--- packages/ui/src/Button/Button.test.tsx
+++ packages/ui/src/Button/Button.test.tsx
@@ -3,10 +3,22 @@ import renderer from 'react-test-renderer';
 import Button from './Button';
 
 describe('Button', () => {
-    it('snapshot is unchanged', () => {
+    it('primary snapshot is unchanged', () => {
         const toRender = (
             <Button>
-                Hello WOrld
+                Hello World
+            </Button>
+        );
+
+        const tree = renderer.create(toRender).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('danger snapshot is unchanged', () => {
+        const toRender = (
+            <Button intent="danger">
+                Hello World
             </Button>
         );
 
