5cb94ca945436b180ee7ca43b32df4a50a6e7fac
MrX8503 <viet_tran@hermanmiller.com>
2023-08-18 11:22:23 -0400

    feature(web-package): add about page

---
+++ apps/web/src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+    return (
+        <div className="ui-container ui-py-10 ui-mx-auto">
+            <div className="ui-max-w-2xl">
+                <h1 className="ui-text-4xl ui-mb-4">About Page</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti obcaecati at similique, tenetur animi omnis dignissimos voluptates, ipsum sint, eligendi mollitia delectus ratione esse sunt quod maiores provident doloremque ea?</p>
+            </div>
+        </div>
+    );
+}
